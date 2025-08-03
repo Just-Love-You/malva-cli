0c302e3cd8ff8ee282ea60ed60b6c664a1761ef2
Andrii Hlybchenko <hlybchenko.andrii@gmail.com>
2025-08-03 20:20:52 +0300

    upd change logic; fixed combo params issues; added more comments

--- cmd/helpers/change_helpers.go
+++ cmd/helpers/change_helpers.go
@@ -35,35 +35,43 @@ func ProcessChange(input string, opts ChangeOptions) error {
 
 	// 4. Prepare video filters: watermark overlay and resize
 	var filterComplex string
-	if opts.Watermark != "" {
+	var hasResize bool = opts.ResizeHeight > 0 || opts.ResizeWidth > 0
+	var hasWatermark bool = opts.Watermark != ""
+	var hasFilter bool = hasResize || hasWatermark
+
+	if hasWatermark {
 		// 4.1. Check watermark file existence
 		if _, err := os.Stat(opts.Watermark); err != nil {
 			return fmt.Errorf("watermark file not found: %s", opts.Watermark)
 		}
-		var baseFilter string
-		if opts.ResizeHeight > 0 || opts.ResizeWidth > 0 {
+
+		// add watermark as second input
+		args = append(args, "-i", opts.Watermark)
+
+		if hasResize {
+			var baseFilter string
 			if opts.ResizeHeight > 0 {
 				baseFilter = fmt.Sprintf("scale=-2:%d", opts.ResizeHeight)
 			} else {
 				baseFilter = fmt.Sprintf("scale=%d:-2", opts.ResizeWidth)
 			}
+			// scale then overlay, final output labeled [v0]
+			filterComplex = fmt.Sprintf("[0:v]%s[scaled];[scaled][1:v]overlay=10:10[v0]", baseFilter)
 		} else {
-			baseFilter = ""
-		}
-
-		if baseFilter != "" {
-			filterComplex = fmt.Sprintf("[0:v]%s[v0];[v0][1:v]overlay=10:10", baseFilter)
-		} else {
+			// just overlay, final output labeled [v0]
 			filterComplex = "[0:v][1:v]overlay=10:10[v0]"
 		}
 
-		args = append(args, "-i", opts.Watermark, "-filter_complex", filterComplex)
+		args = append(args, "-filter_complex", filterComplex)
+
+		// map filtered video
 		args = append(args, "-map", "[v0]")
 
-		if opts.ReplaceAudio == "" {
+		// map audio only if not removed and not replaced
+		if opts.ReplaceAudio == "" && !opts.RemoveAudio {
 			args = append(args, "-map", "0:a")
 		}
-	} else if opts.ResizeHeight > 0 || opts.ResizeWidth > 0 {
+	} else if hasResize {
 		var vf string
 		if opts.ResizeHeight > 0 {
 			vf = fmt.Sprintf("scale=-2:%d", opts.ResizeHeight)
@@ -78,28 +86,29 @@ func ProcessChange(input string, opts ChangeOptions) error {
 		if _, err := os.Stat(opts.ReplaceAudio); err != nil {
 			return fmt.Errorf("audio file not found: %s", opts.ReplaceAudio)
 		}
-		args = append(args,
-			"-i", opts.ReplaceAudio,
-			"-map", "0:v", "-map", "2:a",
-		)
+		args = append(args, "-i", opts.ReplaceAudio)
+
+		// if HAVE watermark 		--> outputs: 0=video, 1=watermark, 2=replaceAudio
+		// if DO NOT HAVE watermark --> outputs: 0=video, 1=replaceAudio
+		if hasWatermark {
+			args = append(args, "-map", "2:a")
+		} else {
+			args = append(args, "-map", "1:a")
+		}
 	}
 
 	// 6. Choose codec and format: copy or re-encode with faststart
-	hasFilter := opts.ResizeHeight > 0 || opts.ResizeWidth > 0 || opts.Watermark != ""
-	if hasFilter {
-		// re-encode video and handle audio
+	if hasFilter || opts.ReplaceAudio != "" {
 		args = append(args, "-c:v", "libx264")
 		if opts.ReplaceAudio != "" {
 			args = append(args, "-c:a", "aac")
-		} else {
+		} else if !opts.RemoveAudio {
 			args = append(args, "-c:a", "copy")
 		}
-		args = append(args, "-movflags", "+faststart")
 	} else {
-		// simple stream copy if no filters
 		args = append(args, "-c", "copy")
-		args = append(args, "-movflags", "+faststart")
 	}
+
 	// 7. Specify output file
 	args = append(args, output)
 
