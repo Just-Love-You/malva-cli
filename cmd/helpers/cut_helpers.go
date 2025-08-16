ef1369c92d9eb7b227b31666de65b0894c20cf49
Andrii Hlybchenko <hlybchenko.andrii@gmail.com>
2025-08-16 22:55:34 +0300

    returned deleted code - FormatDurationFFMPEG

--- cmd/helpers/cut_helpers.go
+++ cmd/helpers/cut_helpers.go
@@ -41,6 +41,16 @@ func ProcessCut(input string, opts CutOptions) error {
 	return cmd.Run()
 }
 
+// FormatDurationFFMPEG converts a time.Duration to "HH:MM:SS.ms" format for ffmpeg
+func FormatDurationFFMPEG(d time.Duration) string {
+	totalMillis := d.Milliseconds()
+	h := totalMillis / 3600000
+	m := (totalMillis % 3600000) / 60000
+	s := (totalMillis % 60000) / 1000
+	ms := totalMillis % 1000
+	return fmt.Sprintf("%02d:%02d:%02d.%03d", h, m, s, ms)
+}
+
 func parseFFmpegTime(hours, mins, sec string) float64 {
 	hh, _ := strconv.Atoi(hours)
 	mm, _ := strconv.Atoi(mins)
